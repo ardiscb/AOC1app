9f3d7fe73b8bd120681b356c79cbd2e5401598c2
Courtney Ardis <ardiscb@fullsail.edu>
2013-01-14 19:53:57 -0500

    Added inital files for Week 2 project.

---
+++ Week2/OBJCIapp2/OBJCIapp2/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  OBJCIapp2
+//
+//  Created by Courtney Ardis on 1/14/13.
+//  Copyright (c) 2013 Courtney Ardis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
