58321c506a18577abf5425e803df371802a802fd
Courtney Ardis <ardiscb@fullsail.edu>
2013-01-16 22:20:04 -0500

    Created UILabel for the Title of Book, centered title and, changed the background color.

--- Week2/AOC1P2app/AOC1P2app/ViewController.m
+++ Week2/AOC1P2app/AOC1P2app/ViewController.m
@@ -16,6 +16,21 @@
 
 - (void)viewDidLoad
 {
+    //Change background to purple
+    self.view.backgroundColor = [UIColor purpleColor];
+
+    //Create bookTitleLabel
+    bookTitleLabel = [[UILabel alloc] initWithFrame:CGRectMake(10.0f, 10.f, 300.0f, 30.f)];
+    //Check to see whether bookTitleLabel is not nil
+    if (bookTitleLabel != nil)
+    {
+        //If not nil, add centered aligned text
+        bookTitleLabel.text = @"The Elric Saga Part 2";
+        bookTitleLabel.textAlignment = NSTextAlignmentCenter;
+    }
+    //Attach bookTitleLabel to ViewController
+    [self.view addSubview:bookTitleLabel];
+    
     
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
